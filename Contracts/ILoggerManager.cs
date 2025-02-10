1a5b5a1afecb740dc3485ae95d50ef49b290d1ff
dasmac <dasmac@MANDOLORIAN773E>
2025-02-10 22:14:06 +0100

    Add project files.

---
+++ Contracts/ILoggerManager.cs
@@ -0,0 +1,11 @@
+﻿namespace Contracts
+{
+    public interface ILoggerManager
+    {
+        void LogInfo(string message);
+        void LogWarning(string message);
+        void LogError(string message);
+        void LogDebug(string message);
+
+    };
+}
---
+++ LoggingService/LoggerManager.cs
@@ -0,0 +1,11 @@
+﻿using Contracts;
+using NLog;
+
+namespace LoggingService
+{
+    public class LoggerManager : ILoggerManager
+    {
+        private readonly ILogger logger => LogManager.GetCurrentClassLogger();
+
+    }
+}
