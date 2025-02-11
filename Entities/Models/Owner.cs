d6d64a46be9388632a1fb4b8ffb33bf27172de9b
David Saga <epostsaga@gmail.com>
2025-02-11 13:16:24 +0100

    Pomelo.EntityFrameworkCore.MySql pack installed, model classes in entity class library created, data annotations modified model properties

---
+++ Entities/Models/Owner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pomelo.EntityFrameworkCore.MySql;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Models
+{
+    [Table("owner")]
+    public class Owner
+    {
+        public Guid OwnerId { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(60, ErrorMessage = "Name can't be longer than 60 characters")]
+        public string Name { get; set; }= string.Empty;
+
+        [Required(ErrorMessage = "Date of birth is required")]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        [StringLength(100, ErrorMessage = "Address can't be longer than 100 characters")]
+        public string Address { get; set; } = string.Empty;
+
+
+        public ICollection<Account>? Accounts { get; set; }
+}
+}
